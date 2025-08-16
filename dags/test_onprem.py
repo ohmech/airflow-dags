07b63c400bf66e412911b536ec85a2e13dc16038
ohmech <ohmech992@gmail.com>
2025-08-16 17:11:06 +0000

    add test_onprem.py

---
+++ dags/test_onprem.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+with DAG(
+    dag_id="test_onprem",
+    start_date=days_ago(1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+    test_task = KubernetesPodOperator(
+        task_id="echo-task",
+        name="echo-task",
+        namespace="default",  # 온프렘 네임스페이스
+        image="alpine:3.20",
+        cmds=["sh", "-c"],
+        arguments=["echo hello-from-onprem && sleep 5"],
+        config_file="/opt/onprem/kubeconfig",  # 게이트웨이 kubeconfig
+        get_logs=True,
+        is_delete_operator_pod=True,
+    )
